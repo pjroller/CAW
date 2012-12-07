cabe941f0a14f37096799f2c2f23fa655998b9ed
Matthew Richmond <matthew@joshua.svacacave.net>
2012-12-06 20:54:40 -0500

    Added a recent posts by thumbnail option into sidebar

--- sidebar.php
+++ sidebar.php
@@ -38,6 +38,37 @@
 
 
 			<?php endif; // end sidebar widget area ?>
+			
+			
+			<aside id="meta" class="widget">
+				<h1 class="widget-title">Foo</h1>
+				<ul>
+				
+					<?php
+						$recent_posts = wp_get_recent_posts();
+						foreach( $recent_posts as $recent ){
+							// echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+							// echo $recent["ID"]. "<br/>";
+							
+							if (has_post_thumbnail( $recent["ID"] ) ):
+								
+								// $image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent["ID"] ), 'single-post-thumbnail' );	
+								// // 
+								// echo '<img src="' . $image[0] . '" />';
+								
+								// echo the_post_thumbnail( array(100,100) );
+								echo '<a href="' . get_permalink( $recent["ID"] ) .'">' . get_the_title( $recent["ID"] ) . '</a>';
+								echo get_the_post_thumbnail( $recent["ID"], 'sm-post-thumb' ) . '<br/>';
+								
+							endif;
+						}
+					?>
+				
+				
+				</ul>
+			</aside>
+			
+			
 		</div><!-- #secondary .widget-area -->
 
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
