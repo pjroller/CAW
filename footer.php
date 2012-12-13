5ec5788d1f94eab8dcec36caf0839813a90e0489
Pam Roller <pamroller@gmail.com>
2012-12-12 22:11:12 -0500

    CSS updates

--- footer.php
+++ footer.php
@@ -13,13 +13,15 @@
 
 	<footer id="colophon" role="contentinfo">
 		<div id="site-generator">
-			<?php do_action( 'toolbox_credits' ); ?>
+				<?php do_action( 'toolbox_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'toolbox' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'toolbox' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'toolbox' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Toolbox', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			
+		<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Toolbox', '<a    href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>    
+		
 		</div>
 	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div><!-- #page -->  
 
 <?php wp_footer(); ?>
 
